6eb1e157656b37d3abc5e583d47b6e8c7844c129
Choiboi500 <jbc8894@gmail.com>
2020-09-28 21:04:43 -0400

    framework not launching

---
+++ settings.sql
@@ -0,0 +1,4 @@
+CREATE DATABASE suresell;
+CREATE USER sureselluser
+WITH PASSWORD 'suresell';
+GRANT ALL PRIVILEGES ON DATABASE suresell TO sureselluser;
--- sureselldjango/settings.py
+++ sureselldjango/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'suresell',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,11 @@ WSGI_APPLICATION = 'sureselldjango.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'suresell',
+        'USER': 'sureselluser',
+        'PASSWORD': 'suresell',
+        'HOST': 'localhost'
     }
 }
 
